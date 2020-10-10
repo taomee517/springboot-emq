97d1765ab67508dfe5840f763350044471d47234
taomee517 <taomee517@gmail.com>
2020-10-10 17:24:39 +0800

    project init

---
+++ src/main/java/org/example/emq/SpringbootEmqApplication.java
@@ -0,0 +1,17 @@
+package org.example.emq;
+
+/**
+ * @author 罗涛
+ * @title SpringbootEmqApplication
+ * @date 2020/10/10 17:09
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEmqApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEmqApplication.class);
+    }
+}
